be507f5ca68c42d3cd1a7c857db4b760c0fe13c5
DiamondJotaku <98100085+DiamondJotaku@users.noreply.github.com>
2023-10-24 16:44:40 +0200

    Moyenne

--- Projet/Mission 3/Facture.js
+++ Projet/Mission 3/Facture.js
@@ -8,3 +8,21 @@ function Result() {
     t16.value = Number(t14.value)*Number(t15.value);
     t18.value = Number(t8.value) + Number(t12.value) + Number(t16.value);
 }
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    const background = document.body;
+    const newColor = getRandomColor();
+    background.style.backgroundColor = newColor;
+}
+
+// Change the background color every 2 seconds (2000 milliseconds)
+setInterval(changeBackgroundColor, 2000);
